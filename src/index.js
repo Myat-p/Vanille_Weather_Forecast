31c81e74d7b9ebf0525cc0456654e91dbbd0a9ef
MyatPP <mpwintphyu555@gmail.com>
2024-10-21 12:14:56 +0630

    Updated css, js and the API for Daily Weather Forecast

--- src/index.js
+++ src/index.js
@@ -17,6 +17,11 @@ timeElement.innerHTML=hour+":"+minute;
 
 
 function weatherforecast(response) {
+
+let city=response.data.city;
+let heading=document.querySelector("h1");
+heading.innerHTML=city;
+
 let temp=Math.round(response.data.temperature.current);
 let degree=document.querySelector(".weather-degree");
 degree.innerHTML=temp;
@@ -38,14 +43,12 @@ let icon=response.data.condition.icon_url;
 let iconElement=document.querySelector(".weather-icon");
 iconElement.innerHTML=`<img src="${icon}">`;
 
+changeDailyWeather(response.data.city);
 }
 
 function displayCity(event){
 event.preventDefault();
 let inputCity=document.querySelector("#weather-city-input").value;
-let heading=document.querySelector("h1");
-heading.innerHTML=inputCity;
-
 let apiKey="20468a8333bf53tb20f4666cf98f1o0b"
 let apiURL=`https://api.shecodes.io/weather/v1/current?query=${inputCity}&key=${apiKey}&units=metric`;
 axios.get(apiURL).then(weatherforecast);
@@ -53,3 +56,36 @@ axios.get(apiURL).then(weatherforecast);
 
 let submitElement=document.querySelector("#weather-submit");
 submitElement.addEventListener("click",displayCity);
+
+function changeDailyWeather(city){
+let apiKey="20468a8333bf53tb20f4666cf98f1o0b";
+let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(DisplayForecast);
+}
+
+function DayWeather(time){
+let date=new Date(time*1000);
+let day=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+return(day[date.getDay()]);
+}
+
+function DisplayForecast(response){
+
+let weatherInfo="";
+console.log(response.data);
+response.data.daily.forEach(function(day,index){
+if (index < 5){
+weatherInfo=weatherInfo + `<div class="weatherdailyForecast">
+	<div class="weatherday">${DayWeather(day.time)}</div>
+	<div><img src="${day.condition.icon_url}" class="weatherdaily-icon"></div>
+	<div class="weatherdaily-temperature"><span class="high-degree">${Math.round(day.temperature.maximum)}</span><span class="low-degree">${Math.round(day.temperature.minimum)}</span></div>
+	</div>`;
+
+let weatherDailyElement=document.querySelector("#weatherForecast");
+weatherDailyElement.innerHTML=weatherInfo;
+}
+});
+}
+
+changeDailyWeather("Burma");
